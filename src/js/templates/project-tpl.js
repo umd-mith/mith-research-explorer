41b4c2469dad52d8813fd91acb4ef74015b7cdcb
Raff Viglianti <raffaeleviglianti@gmail.com>
2015-11-12 13:29:22 -0500

    fixed links by referencing "slug" instead of "name"

--- src/js/templates/project-tpl.js
+++ src/js/templates/project-tpl.js
@@ -4,7 +4,7 @@ let project_tpl = `
 <div class="fusion-portfolio-post mith-research-post fusion-col-spacing fusion-image-landscape">
     <div class="fusion-portfolio-content-wrapper mith-research-content-wrapper" style="opacity: 1">
         <div class="fusion-image-wrapper" aria-haspopup="true">
-            <a href="/research/{{name}}">
+            <a href="/research/{{slug}}">
                 <img width="669" height="200" 
                      src="{{thumbnail}}" 
                      class="attachment-full wp-post-image" />
@@ -12,7 +12,7 @@ let project_tpl = `
         </div>
         <div class="fusion-portfolio-content mith-research-content">
             <h2 class="entry-title">
-                <a href="/research/{{name}}/">
+                <a href="/research/{{slug}}/">
                     {{title}}
                 </a>
             </h2>
