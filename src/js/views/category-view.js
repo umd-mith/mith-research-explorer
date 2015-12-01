umd-mith/mith-research-explorer

commit 3c3474aaf339ddcaf90d168722238f4f19d311e5

Fixed count of projects in a given category
The count needed to be recursive in order to account for all projects in nested categories.

Fixes #14

src/js/views/category-view.js
@@ -92,16 +92,28 @@ class CategoryView extends Backbone.View {
     }
     render() {
         let containedProjects = new Set();
-        if (this.subset){
-            this.subset.each(function(subcat){
-                subcat.get("projects").each(function(project){
-                    containedProjects.add(project.get("slug"));
+
+        let countSubset = function (md) {
+            if (md.subset){
+                md.subset.each(function(subcat){
+                    subcat.get("projects").each(function(project){
+                        containedProjects.add(project.get("slug"));
+                    });
+
+                    if (subcat.get("subset")) {
+                        countSubset(subcat);
+                    }
+
                 });
-            });
+            }
         }
+
+        countSubset(this.model);
+        
         this.model.get("projects").each(function(project){
             containedProjects.add(project.get("slug"));
         });
+
         this.model.set("totProjects", containedProjects.size)
 
         // Don't render if there are no contained projects

commit 3d4321178a9e347d1a9b451ed04c89d2f01f391e

Fixed project count bug introduced with 3c3474a.
Now the recursive function correctly get "subset" as an attribute, not as a property.

Fixes #14

src/js/views/category-view.js
@@ -105,8 +105,8 @@ class CategoryView extends Backbone.View {
         let containedProjects = new Set();
 
         let countSubset = function (md) {
-            if (md.subset){
-                md.subset.each(function(subcat){
+            if (md.get("subset")){
+                md.get("subset").each(function(subcat){
                     subcat.get("projects").each(function(project){
                         containedProjects.add(project.get("slug"));
                     });