6e728c51c97e527632e60677fbe75cc89ebbcaf9
kkeister <kirsten.e.keister@gmail.com>
2015-12-07 15:47:10 -0500

    checkbox sprite image update and add title attribute to label

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -2,10 +2,10 @@ import * as Handlebars from 'handlebars';
 
 let category_tpl = `
 <dd class="cat_count">{{totProjects}}</dd>
-<dt><a href="#" class="only_cat">(only)<a/>
+<dt><a href="#" class="only_cat">only<a/>
 <span class="cb-wrapper">
 	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input" checked></a>
-	<label class="cb-label">{{name}}</label>
+	<label class="cb-label" title="{{name}}">{{name}}</label>
 </span></dt>`
 
 export default Handlebars.compile(category_tpl);
