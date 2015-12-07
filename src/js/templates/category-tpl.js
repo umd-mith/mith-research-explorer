a26f09fd7e0a50cc160418e0847bb63b95c0fdd4
kkeister <kirsten.e.keister@gmail.com>
2015-12-07 15:09:50 -0500

    create css and change html so filter lists display properly

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -1,7 +1,11 @@
 import * as Handlebars from 'handlebars';
 
 let category_tpl = `
-<dt><input type="checkbox" class="toggle_cat" checked> {{name}} <a href="#" class="only_cat">(only)<a/></dt>
-<dd>{{totProjects}}</dd>`
+<span class="cat_count">{{totProjects}}</span>
+<a href="javascript:void(0);" class="only_cat">(only)<a/>
+<span class="cb-wrapper">
+	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input" checked></a>
+	<label for="class="cb-label">{{name}}</label>
+</span>`
 
 export default Handlebars.compile(category_tpl);
