9b930aeeb5e8020cfa87e4cbac24d7288edab9c5
kkeister <kirsten.e.keister@gmail.com>
2015-12-07 15:26:56 -0500

    changing markup back to <dd> and <dt>

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -1,11 +1,11 @@
 import * as Handlebars from 'handlebars';
 
 let category_tpl = `
-<span class="cat_count">{{totProjects}}</span>
-<a href="javascript:void(0);" class="only_cat">(only)<a/>
+<dd class="cat_count">{{totProjects}}</dd>
+<dt><a href="#" class="only_cat">(only)<a/>
 <span class="cb-wrapper">
 	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input" checked></a>
-	<label for="class="cb-label">{{name}}</label>
-</span>`
+	<label class="cb-label">{{name}}</label>
+</span></dt>`
 
 export default Handlebars.compile(category_tpl);
