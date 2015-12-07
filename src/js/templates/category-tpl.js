0097d2fe2d07a663ce1b0ed79ad9a7a62b0c6459
kkeister <kirsten.e.keister@gmail.com>
2015-12-07 16:34:29 -0500

    add caret to top level categories

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -2,10 +2,11 @@ import * as Handlebars from 'handlebars';
 
 let category_tpl = `
 <dd class="cat_count">{{totProjects}}</dd>
-<dt><a href="#" class="only_cat">only<a/>
+<dt><i class="fa fa-caret-up"></i><a href="#" class="only_cat">only<a/>
 <span class="cb-wrapper">
 	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input" checked></a>
 	<label class="cb-label" title="{{name}}">{{name}}</label>
-</span></dt>`
+</span></dt>
+`
 
 export default Handlebars.compile(category_tpl);
