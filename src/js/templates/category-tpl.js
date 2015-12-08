8a24f7442d40d5b128fe5cb1ccc11c0d48c045f3
raffazizzi <raffaeleviglianti@gmail.com>
2015-12-08 17:06:10 -0500

    Now template does not check the boxes by default

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -1,7 +1,7 @@
 import * as Handlebars from 'handlebars';
 
 let category_tpl = `
-<dt><input type="checkbox" class="toggle_cat" checked> {{name}} <a href="#" class="only_cat">(only)<a/></dt>
+<dt><input type="checkbox" class="toggle_cat"> {{name}} <a href="#" class="only_cat">(only)<a/></dt>
 <dd>{{totProjects}}</dd>`
 
 export default Handlebars.compile(category_tpl);
