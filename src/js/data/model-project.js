2db1f9a8790cc0016304ec893fc1ca7f0052bb82
raffazizzi <raffaeleviglianti@gmail.com>
2015-12-10 12:13:19 -0500

    Fixed bug introduced with previous commit that got in the way of counting projects per category correctly.

--- src/js/data/model-project.js
+++ src/js/data/model-project.js
@@ -4,22 +4,12 @@ import * as _ from 'underscore';
 class Project extends Backbone.Model {
 	// Set all categories as active on creation
 	parse(resp, options){             
-        if (!resp.research_type) {
-            resp.research_type = ["Other"];
+        if (resp.research_type) {
+            if (resp.research_type.indexOf("Active") != -1) {
+                resp["active"] = true;
+            }            
         }
-        if (!resp.research_sponsor) {
-            resp.research_type = ["Other"];
-        }
-        if (!resp.topic) {
-            resp.research_type = ["Other"];
-        }
-        if (!resp.start) {
-            resp.research_type = "0000";
-        }
-
-        if (resp.research_type.indexOf("Active") != -1) {
-            resp["active"] = true;
-        }
+        
 		return resp;
 	}
 }
