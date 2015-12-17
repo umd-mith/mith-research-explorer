42ca3b7cb9de14e5d62ab4a250d4a7d291fe1e4f
raffazizzi <raffaeleviglianti@gmail.com>
2015-12-17 15:56:46 -0500

    Removed default checked attribute again

--- src/js/templates/category-tpl.js
+++ src/js/templates/category-tpl.js
@@ -4,7 +4,7 @@ let category_tpl = `
 <dd class="cat_count">{{totProjects}}</dd>
 <dt><i class="fa fa-caret-up"></i><a href="#" class="only_cat">only<a/>
 <span class="cb-wrapper">
-	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input" checked></a>
+	<a href="#" class="cb-icon"><input type="checkbox" class="toggle_cat cb-input"></a>
 	<label class="cb-label" title="{{name}}">{{name}}</label>
 </span></dt>
 `
